3ff38e5048d389af9b3426f649f66390b2ac25eb
Murari Varma <murari23@gmail.com>
2026-04-10 13:21:23 +0530

    deleted unncessary

--- install_deps.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-    console.log("Installing LangChain dependencies...");
-    const msg = execSync('npm install @langchain/core@latest @langchain/google-genai@latest --legacy-peer-deps', { stdio: 'inherit' });
-    console.log("Install completed!");
-} catch (e) {
-    console.error("Failed", e);
-}
